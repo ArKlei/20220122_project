195a34c1e16419f9ef3438fc8f6ae6a76b2ea3dd
ArKlei <arunas.kleinas@gmail.com>
2022-01-22 08:31:28 +0200

    Made ready welcome page

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,59 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+//Pavienis kelias
 Route::get('/', function () {
     return view('welcome');
-});
+  })->name('main');
+
+    Route::prefix('clients')->group(function() {
+    //Index
+    Route::get('', 'App\Http\Controllers\ClientController@index')->name('client.index');
+    //Create
+    Route::get('create', 'App\Http\Controllers\ClientController@create')->name('client.create');
+    Route::post('store', 'App\Http\Controllers\ClientController@store' )->name('client.store');
+    // Edit form, id
+    Route::get('edit/{client}', 'App\Http\Controllers\ClientController@edit')->name('client.edit');
+    Route::post('update/{client}', 'App\Http\Controllers\ClientController@update')->name('client.update');
+    //Delete
+    Route::post('destroy/{client}', 'App\Http\Controllers\ClientController@destroy' )->name('client.destroy');
+    //Show
+    Route::get('show/{client}', 'App\Http\Controllers\ClientController@show')->name('client.show');
+
+  });
+
+
+  Route::prefix('companies')->group(function() {
+
+        //Index
+        Route::get('', 'App\Http\Controllers\CompanyController@index')->name('company.index');
+        //Create
+        Route::get('create', 'App\Http\Controllers\CompanyController@create')->name('company.create');
+        Route::post('store', 'App\Http\Controllers\CompanyController@store' )->name('company.store');
+        //Edit form, id
+        Route::get('edit/{company}', 'App\Http\Controllers\CompanyController@edit')->name('company.edit');
+        Route::post('update/{company}', 'App\Http\Controllers\CompanyController@update')->name('company.update');
+        //Delete
+        Route::post('destroy/{company}', 'App\Http\Controllers\CompanyController@destroy' )->name('company.destroy');
+        //Show
+        Route::get('show/{company}', 'App\Http\Controllers\CompanyController@show')->name('company.show');
+
+  });
+
+  Route::prefix('types')->group(function() {
+
+        //Index
+        Route::get('', 'App\Http\Controllers\TypeController@index')->name('type.index');
+        //Create
+        Route::get('create', 'App\Http\Controllers\TypeController@create')->name('type.create');
+        Route::post('store', 'App\Http\Controllers\TypeController@store' )->name('type.store');
+        //Edit form, id
+        Route::get('edit/{type}', 'App\Http\Controllers\TypeController@edit')->name('type.edit');
+        Route::post('update/{type}', 'App\Http\Controllers\TypeController@update')->name('type.update');
+        //Delete
+        Route::post('destroy/{type}', 'App\Http\Controllers\TypeController@destroy' )->name('type.destroy');
+        //Show
+        Route::get('show/{type}', 'App\Http\Controllers\TypeController@show')->name('type.show');
+  
+  });
+
